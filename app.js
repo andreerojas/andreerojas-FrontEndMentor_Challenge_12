cd4e5601607260d1275feb8184df770ea06aa067
Andree Rojas <andre_r_g@hotmail.com>
2022-10-12 16:57:01 +0200

    active states added  and slider background function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const numViews = document.querySelector('#num-views');
 const slider = document.querySelector('input[type="range"]');
 const price = document.querySelector('.price');
 const billingFreq = document.querySelector('.bill-frequency');
-const selector = document.querySelector('input[type="checkbox"');
+const selector = document.querySelector('input[type="checkbox"]');
 const inputs = document.querySelectorAll('input');
 const prices = 
 [
@@ -19,24 +19,32 @@ function fillNumViews() {
 function fillPrice(){
     //selector checked means yearly billing
     if(selector.checked){
-        price.innerText = `$${prices[slider.value]["price"]*12*0.75} `;
+        price.innerText = `$${prices[slider.value]["price"]*12*0.75}.00`;
         billingFreq.innerText = "/ year";
     }
     else{
-        price.innerText = `$${prices[slider.value]["price"]} `;
+        price.innerText = `$${prices[slider.value]["price"]}.00`;
         billingFreq.innerText = "/ month";
     }
     
 }
 
+function styleSlider(){
+    let value = `${(slider.value-slider.min)/(slider.max-slider.min)*100}%`;
+    slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${value}, hsl(224, 65%, 95%) ${value})`;
+}
+
 function fillData(){
     fillNumViews();
     fillPrice();
-}
+    styleSlider();
 
+}
 
 for(let input of inputs){
     input.addEventListener('input', fillData);
 }
 
 fillData();
+
+
